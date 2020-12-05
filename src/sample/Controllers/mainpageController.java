49518a3e544be3fa31b2a533cfe2946d506b7e6c
Zichen901 <Zichen901@gmail.com>
2020-12-05 16:05:48 -0600

    Able to insert data into MySql after clicking on the save button, still havent save the userId from the User table yet.

--- src/sample/Controllers/mainpageController.java
+++ src/sample/Controllers/mainpageController.java
@@ -1,20 +1,32 @@
 package sample.Controllers;
 
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
+import com.mysql.jdbc.Statement;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import org.json.JSONObject;
+import sample.Models.Country;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.sql.DriverManager;
 import java.util.ResourceBundle;
+import java.util.UUID;
 
 
 public class mainpageController implements Initializable{
 
+    //Strings to use to connect to the AWS MySQL server.
+    final String hostname = "jdbc:mysql://class3368.c3qkvsmzsjaa.us-east-1.rds.amazonaws.com:3306/CovidTrack";
+    final String rdbusername = "admin";
+    final String rdbpassword ="IAmNotAdmin169!";
+
+    //Strings to REQUEST from API
     String apiurl ="https://covid-19-tracking.p.rapidapi.com/v1/";
     String apikey = "af085016f0mshb822eda0980b34fp1265d5jsne7d490a3ea89";
 
@@ -37,6 +49,9 @@ public class mainpageController implements Initializable{
 
     @FXML
     private Button addUserButton;
+
+    @FXML
+    private Button saveButton;
 
     //Textfield from mainpage.fxml
     @FXML
@@ -66,7 +81,11 @@ public class mainpageController implements Initializable{
             }
             in.close();
             //end of credit
+
+            //Creating Objects
             JSONObject countryresponse= new JSONObject(response.toString());
+
+            //Setting lables to JSON key values.
             countryLable.setText(countryresponse.getString("Country_text"));
             dateLable.setText(String.valueOf(java.time.LocalDate.now()));
             tcaseLable.setText(countryresponse.getString("Total Cases_text"));
@@ -74,8 +93,72 @@ public class mainpageController implements Initializable{
             dcaseLable.setText(countryresponse.getString("New Cases_text"));
 
 
-
         }catch (Exception e){
+            Alert invalidCountry = new Alert(Alert.AlertType.ERROR);
+            invalidCountry.setTitle("Error");
+            invalidCountry.setHeaderText("Must provide country name.");
+            invalidCountry.show();
+        }
+
+
+
+
+    }
+
+    private void saveCountry(String uInput, String apiurl, String apikey, String sqlurl, String username, String password){
+        //GET request from API and creating country object.
+        try {
+            //Credit to jinu jawad m on Youtube.
+            URL jObj = new URL(apiurl + uInput);
+            HttpURLConnection httpCon = (HttpURLConnection) jObj.openConnection();
+            httpCon.setRequestProperty("x-rapidapi-key", apikey);
+            int responseCode = httpCon.getResponseCode();
+
+            System.out.println("Sending GET request to " + apiurl + uInput);
+            System.out.println("Response Code " + responseCode);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(httpCon.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            //end of credit
+
+            //Creating country object.
+            Country searchCountry = new Country();
+            JSONObject countryresponse= new JSONObject(response.toString());
+
+            //Inserting values into Country object
+            searchCountry.setCountryid(String.valueOf(UUID.randomUUID().toString()));
+            searchCountry.setName(countryresponse.getString("Country_text"));
+            searchCountry.setDate(String.valueOf(java.time.LocalDate.now()));
+            searchCountry.setTotCase(countryresponse.getString("Total Cases_text"));
+            searchCountry.setTotDeaths(countryresponse.getString("Total Deaths_text"));
+            searchCountry.setNewCase(countryresponse.getString("New Cases_text"));
+
+            //Connecting to SQL database.
+            Connection conn = (Connection) DriverManager.getConnection(sqlurl, username, password);
+            //Inserting objects from list to table. Credit to Alvin Alexander on alvinalexander.com
+            String query = " insert into CovidCount (Id, CountryName, Date, TotalCases, TotalDeaths, NewCases)"
+                    + " values (?, ?, ?, ?, ?, ?)";
+            PreparedStatement pStmt = (PreparedStatement) conn.prepareStatement(query);
+
+            pStmt.setString(1, searchCountry.getCountryid());
+            pStmt.setString(2, searchCountry.getName());
+            pStmt.setString(3, searchCountry.getDate());
+            pStmt.setString(4, searchCountry.getTotCase());
+            pStmt.setString(5, searchCountry.getTotDeaths());
+            pStmt.setString(6, searchCountry.getNewCase());
+            //execute prepared statment.
+            pStmt.execute();
+            //closing prepare statment.
+            pStmt.close();
+            //closing connections
+            conn.close();
+
+        }catch(Exception e){
             Alert invalidCountry = new Alert(Alert.AlertType.ERROR);
             invalidCountry.setTitle("Error");
             invalidCountry.setHeaderText("Must provide country name.");
@@ -95,6 +178,12 @@ public class mainpageController implements Initializable{
             String userInput = countryName.getText();
             readJson(userInput, apiurl, apikey );
         });
+
+        saveButton.setOnAction((event)->{
+            String userInput = countryName.getText();
+            saveCountry(userInput, apiurl, apikey, hostname, rdbusername, rdbpassword);
+        });
+
 
     }
 }
--- src/sample/Models/Country.java
+++ src/sample/Models/Country.java
@@ -1,6 +1,72 @@
 package sample.Models;
 
+
 public class Country {
+    private String countryid;
+    private String name;
+    private String date;
+    private String TotCase;
+    private String TotDeaths;
+    private String NewCase;
 
+    public void Country(String countryid, String name, String date, String TotCase, String TotDeaths, String NewCase){
+        this.countryid = countryid;
+        this.name = name;
+        this.date = date;
+        this.TotCase = TotCase;
+        this.TotDeaths = TotDeaths;
+        this.NewCase = NewCase;
+    }
 
+    public void Country(){
+
+    }
+
+    public String getCountryid() {
+        return countryid;
+    }
+
+    public void setCountryid(String countryid) {
+        this.countryid = countryid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTotCase() {
+        return TotCase;
+    }
+
+    public void setTotCase(String totCase) {
+        TotCase = totCase;
+    }
+
+    public String getTotDeaths() {
+        return TotDeaths;
+    }
+
+    public void setTotDeaths(String totDeaths) {
+        TotDeaths = totDeaths;
+    }
+
+    public String getNewCase() {
+        return NewCase;
+    }
+
+    public void setNewCase(String newCase) {
+        NewCase = newCase;
+    }
 }
